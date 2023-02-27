6fa9c0e7869ae3b452d38737f8a9ff62a9379d47
Saki <sbasken@gmail.com>
2023-02-26 21:37:39 -0800

    complete get and post fetch for the app using useEffect and for form

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
-  const [page, setPage] = useState("List");
+  const [ page, setPage ] = useState("List");
+  const [ questions, setQuestions ] = useState([])
+
+  const handleAddQuestions = (newQuestion) => {
+    setQuestions([...questions, newQuestion])
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then(res => res.json())
+    .then(questionList => setQuestions(questionList))
+  }, [])
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm onAddQuestions={handleAddQuestions} /> : <QuestionList questions={questions}/>}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,14 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({ questions }) {
+  
+  const renderQuestions = questions.map( question => <QuestionItem key={question.id} question={question}/>)
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{renderQuestions}</ul>
     </section>
   );
 }
